c96b6e99dba378cc85be3bff11fcfb1963559be9
thaynafonseca-mytech <thayna.fonseca@mytech.com.br>
2024-07-30 17:59:04 -0300

    Fix: terraform

--- vpc/vpc-endpoint/main.tf
+++ vpc/vpc-endpoint/main.tf
@@ -1,9 +1,16 @@
+locals {
+  tags = {
+    ManagedBy   = "Terraform"
+  }
+}
+
 module "endpoints" {
   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.9.0"
 
   vpc_id                = var.vpc_id
   subnet_ids            = var.subnet_ids
   security_group_ids    = var.security_group_ids
   endpoints             = var.endpoints
-  tags                  = var.tags
+  tags                  = local.tags
 }
--- vpc/vpc-endpoint/variables.tf
+++ vpc/vpc-endpoint/variables.tf
@@ -1,13 +1,14 @@
 variable "vpc_id" {
   description = "The ID of the VPC"
   type        = string
+  default = ""
 }
 
 variable "subnet_ids" {
   description = "Subnets IDs to associate with the VPC endpoints"
   type        = list(string)
   default     = []
-}
+} 
 
 variable "security_group_ids" {
   description = "List of security group IDs"
@@ -23,9 +24,11 @@ variable "endpoints" {
     private_dns_enabled = optional(bool)
     tags                = map(string)
   }))
+  default = {}
 }
 
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
+  default = {}
 }
